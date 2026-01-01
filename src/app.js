afd56f0bd90debc6e2fddcec0aa35d8c334540fb
Bruno H. Homem <brunohhomem@gmail.com>
2026-01-01 16:24:26 -0300

    feat: Validacao da variavel de ambiente PORT e adiciona `dotenv` como dependencia do projeto.

--- src/app.js
+++ src/app.js
@@ -1,44 +1,54 @@
-'use strict';
-const cors = require('cors');
-const SwaggerExpress = require('swagger-express-mw');
-const SwaggerUi = require('swagger-ui-express');
-const express = require('express');
-const { buildHandlers } = require('./modules');
-const { handlers } = buildHandlers();
-const port = Number(process.env.PORT || 8089)
+'use strict'
 
-const app = express();
+require('dotenv').config()
+
+const cors = require('cors')
+const SwaggerExpress = require('swagger-express-mw')
+const SwaggerUi = require('swagger-ui-express')
+const express = require('express')
+const { buildHandlers } = require('./modules')
+const { handlers } = buildHandlers()
+
+const port = Number(process.env.PORT)
+
+if (!port || Number.isNaN(port)) {
+  throw new Error('Environment variable PORT is required and must be a number')
+}
+
+const app = express()
 
 const whitelist = [
   // TODO whitelist
 ]
 
-app.use(cors({
-  origin: function (origin, callback) {
-    const allowed = whitelist.indexOf(origin) !== -1
-    if (allowed) return callback(null, true);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowed = whitelist.indexOf(origin) !== -1
+      if (allowed) return callback(null, true)
 
-    callback(new Error('Not allowed by CORS'))
-  }
-}))
+      callback(new Error('Not allowed by CORS'))
+    }
+  })
+)
 
 const swaggerConfig = {
   appRoot: __dirname,
-  swaggerFile: `${__dirname}/config/swagger.yaml`,
-};
+  swaggerFile: `${__dirname}/config/swagger.yaml`
+}
 
 const onSwaggerCreated = (error, swaggerExpress) => {
-  if (error) throw error;
+  if (error) throw error
 
-  const swaggerDocument = swaggerExpress.runner.swagger;
-  app.use('/api/v1/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
-  swaggerExpress.register(app); // register middlewares
-  app.listen(port, () => console.info('onAppStart', { port }));
-};
+  const swaggerDocument = swaggerExpress.runner.swagger
+  app.use('/api/v1/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument))
+  swaggerExpress.register(app) // register middlewares
+  app.listen(port, () => console.info('onAppStart', { port }))
+}
 
-SwaggerExpress.create(swaggerConfig, onSwaggerCreated);
+SwaggerExpress.create(swaggerConfig, onSwaggerCreated)
 
 module.exports = {
   app,
   ...handlers
-};
+}
