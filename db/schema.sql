6fe40a88a631eb7064f3424ed37317a539385485
Bruno H. Homem <brunohhomem@gmail.com>
2026-01-02 11:44:39 -0300

    fix: Correcao do script das tabelas do banco de dados

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,18 @@
 USE `main`;
 
-CREATE TABLE posts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    author_id INT NOT NULL,
-    post_text VARCHAR(200)
-);
-
 CREATE TABLE users (
-    id VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100),
-    user_email VARCHAR(100) NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
     user_password VARCHAR(200) NOT NULL
 );
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root';
-
-FLUSH PRIVILEGES;
-
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT NOT NULL,
+    post_text TEXT,
+    CONSTRAINT fk_posts_user
+        FOREIGN KEY (author_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
