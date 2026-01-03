d0e91946c867ba38927895190d7d44fa2b41e87e
Bruno H. Homem <brunohhomem@gmail.com>
2026-01-03 16:50:30 -0300

    fix: Ajustes no retorno do endpoint de criar usuario

--- src/modules/services/User/createUserService.js
+++ src/modules/services/User/createUserService.js
@@ -1,25 +1,16 @@
 const bcrypt = require('bcryptjs')
 const salt = bcrypt.genSaltSync(10)
-const { createUserRepositories } = require('../../repositories')
+const { createUserRepository } = require('../../repositories')
 
 const createUserService = async user => {
   const crypt_password = bcrypt.hashSync(user.user_password, salt)
 
-  const { user_created } = await createUserRepositories({
+  const user_created = await createUserRepository({
     user: {
       ...user,
       user_password: crypt_password
     }
   })
-
-  const has_user_created =
-    Array.isArray(user_created) && user_created.length > 0
-
-  if (has_user_created === false) {
-    return {
-      user_created_id: []
-    }
-  }
 
   return {
     user_created_id: user_created
