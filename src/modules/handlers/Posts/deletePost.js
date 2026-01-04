50398a1cf0626bf455f1f6229a6cb2ed8f3bda65
Bruno H. Homem <brunohhomem@gmail.com>
2026-01-04 07:28:30 -0300

    fix: Correcao do endpoint de remocao de posts

--- src/modules/handlers/Posts/deletePost.js
+++ src/modules/handlers/Posts/deletePost.js
@@ -1,28 +1,27 @@
 'use strict'
 
-const httpStatusCodes = require('http-status-codes');
-const { httpErrorHandler } = require("../../common/handlers");
-const { deletePostService } = require('../../services');
+const httpStatusCodes = require('http-status-codes')
+const { httpErrorHandler } = require('../../common/handlers')
+const { deletePostService } = require('../../services')
 
 const deletePostHandler = async (req, res, next) => {
-    try{
+  try {
+    const { post_id } = req.query
 
-        const {
-            post_id
-        } = req.query;
-
-        const {
-            deletedPost
-        } = await deletePostService({
-            post_id
-        })
-
-        return res.status(httpStatusCodes.OK).send({deletedPost})
-    }catch(error){
-        return httpErrorHandler({ req, res, error })
+    if (!post_id) {
+      return res.status(httpStatusCodes.BAD_REQUEST).send({
+        message: 'post_id is required'
+      })
     }
+
+    await deletePostService({ post_id })
+
+    return res.status(httpStatusCodes.OK).send()
+  } catch (error) {
+    return httpErrorHandler({ req, res, error })
+  }
 }
 
 module.exports = {
-    deletePostHandler
+  deletePostHandler
 }
--- src/modules/services/Post/deletePostService.js
+++ src/modules/services/Post/deletePostService.js
@@ -1,28 +1,16 @@
 const {
-  getPostByPostIdRepositories,
-  deletePostRepositories
+  getPostByPostIdRepository,
+  deletePostRepository
 } = require('../../repositories')
 
 const deletePostService = async ({ post_id }) => {
-  const { posts = [] } = await getPostByPostIdRepositories({
-    post_id
-  })
+  const { post } = await getPostByPostIdRepository({ post_id })
 
-  const has_post = Array.isArray(posts) && posts.length === 1
-
-  if (!has_post) {
+  if (!post) {
     throw new Error("Hasn't post to delete")
   }
 
-  const [post_to_delete] = posts
-
-  await deletePostRepositories({
-    post_id: post_to_delete.id
-  })
-
-  return {
-    deletedPost: post_to_delete
-  }
+  await deletePostRepository({ post_id })
 }
 
 module.exports = {
